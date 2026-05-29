44abee5a4ab69cdf6f374b8e2e2737d690ca8cd9
aanchal <259937850+aanchal-devspace@users.noreply.github.com>
2026-05-29 14:53:04 +0530

    First commit

---
+++ Day-1/main.cpp
@@ -0,0 +1,13 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main(){
+    int sum=0,i,n;
+    cout << "enter a number ";
+    cin >> n ;
+    for(i=1;i<=n;i++){
+        sum+=i;
+    }
+    cout << "the sum is"<< sum<< endl;
+    return 0;
+
+}
